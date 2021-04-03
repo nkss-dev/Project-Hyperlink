b37e41eb2bc50cd1ae45b49cfdfc58e7e42aed2a
GetPsyched <priyanshutr@gmail.com>
2021-04-03 18:47:33 +0400

    Fixed bugs
    - Fixed a bug where games with spaces could not be entered
    
    - Optimised the code

--- ign.py
+++ ign.py
@@ -1,146 +1,152 @@
 import sqlite3, json, os, discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
 class IGN(commands.Cog):
     def __init__(self):
         self.conn = sqlite3.connect('db/details.db')
         self.c = self.conn.cursor()
+        try:
+            with open('db/games.json') as f:
+                self.data = json.load(f)
+        except FileNotFoundError:
+            self.data = {}
 
     @commands.group(name='ign')
     async def ign(self, ctx):
         # Gets details of user from the database
-        self.c.execute('SELECT * FROM main where Discord_UID = (:uid)', {'uid': ctx.author.id})
+        self.c.execute('SELECT Verified FROM main where Discord_UID = (:uid)', {'uid': ctx.author.id})
         tuple = self.c.fetchone()
-        if tuple[11] == 'False':
-            await ctx.send(f'Only members with a verified email can use this command, {ctx.author.mention}.')
+        if tuple[0] == 'False':
+            await ctx.reply('Only members with a verified email can use this command.')
             raise Exception('Permission Denied (Absence of a verified email)')
         if not ctx.invoked_subcommand:
-            await ctx.send('Invalid IGN command passed.')
+            await ctx.reply('Invalid IGN command passed.')
 
     @ign.command(name='add')
-    async def add(self, ctx):
-        # Assigns message content to variable
-        try:
-            content = ctx.message.content.split('add ')[1].split(' ')
-        except:
-            # Sends an embed with a list of all available games
+    async def add(self, ctx, *args):
+        # Loads the available games from the database
+        games = self.data[str(ctx.guild.id)]
+        if not args:
             msg = ''
-            for i in json.loads(os.getenv('Games')):
+            for i in games:
                 msg += f'\n{i}'
+            # Sends an embed with a list of all available games
             embed = discord.Embed(
                 title = 'Here is a list of the games that you can add an IGN for:',
                 description = msg,
                 color = discord.Colour.blurple()
             )
-            await ctx.send(embed = embed)
+            await ctx.send(embed=embed)
+            return
+        # Exit if the game does not exist in the database
+        if args[0] not in games:
+            await ctx.reply(f'The entered game does not exist in the database. If you want it added, contact a moderator.\nFor a list of available games, type `{prefix}ign add`')
             return
         # Exit if IGN is missing
-        try:
-            ign = content[1]
-        except:
-            await ctx.send('Missing arguements')
+        if len(args) < 2:
+            await ctx.reply('Missing arguements')
             return
-        # Checks if the game exists in the database
-        games = json.loads(os.getenv('Games'))
-        if content[0] not in games:
-            await ctx.send(f'The entered game does not exist in the database, {ctx.author.mention}. If you want it added, contact a moderator.')
+        if '@everyone' in args[1] or '@here' in args[1]:
+            await ctx.reply('It was worth a try.')
             return
         # Gets details of user from the database
-        self.c.execute('SELECT * FROM main where Discord_UID = (:uid)', {'uid': ctx.author.id})
+        self.c.execute('SELECT IGN FROM main where Discord_UID = (:uid)', {'uid': ctx.author.id})
         tuple = self.c.fetchone()
-        igns = json.loads(tuple[14])
+        igns = json.loads(tuple[0])
         # Adds IGN to the database
-        igns[content[0]] = content[1]
+        igns[args[0]] = args[1]
         self.c.execute('UPDATE main set IGN = (:ign) where Discord_UID = (:uid)', {'ign': json.dumps(igns), 'uid': ctx.author.id})
         self.conn.commit()
-        await ctx.send(f'IGN for {content[0]} added successfully, {ctx.author.mention}.')
+        await ctx.reply(f'IGN for {args[0]} added successfully.')
 
     @ign.command(name='show')
-    async def show(self, ctx):
-        # Assigns message content to variable
-        try:
-            content = ctx.message.content.split('show ')[1]
-        except:
-            content = str(ctx.author)
+    async def show(self, ctx, *args):
+        # Setting single to False will show all the IGNs for the requested user
+        if not args:
             member = ctx.author
-        # Checks if the game exists in the database
-        try:
-            game = content.split(' ')[1]
-            games = json.loads(os.getenv('Games'))
-            if game not in games:
-                await ctx.send(f'The entered game does not exist in the database, {ctx.author.mention}. If you want it added, contact a moderator.\nFor a list of available games, type `{prefix}ign add`')
-                return
-            content = content.split(' ')[0]
-            single = True
-        except:
             single = False
-        # Formats the usertag and puts it in a variable
-        try:
-            user = content.replace('`', '').replace('@', '')
-        except:
-            await ctx.send('Missing arguement: Usertag')
+        elif 'all' in args[0].lower():
+            single = False
+        else:
+            single = True
+        # Loads the available games from the database
+        games = self.data[str(ctx.guild.id)]
+        # Exit if the game does not exist in the database
+        if single and args[0] not in games:
+            await ctx.reply(f'The entered game does not exist in the database. If you want it added, contact a moderator.\nFor a list of available games, type `{prefix}ign add`')
             return
-        member = ctx.guild.get_member_named(user)
+        oneself = True
+        if len(args) == 1:
+            member = ctx.author
+            game = args[0]
+        elif len(args) == 2:
+            member = ctx.guild.get_member_named(args[1])
+            if not member:
+                await ctx.reply(f'Member with the name `{args[1]}` not found!')
+                return
+            game = args[0]
+            oneself = False
         # Gets details of user from the database
-        self.c.execute('SELECT * FROM main where Discord_UID = (:uid)', {'uid': member.id})
+        self.c.execute('SELECT IGN FROM main where Discord_UID = (:uid)', {'uid': member.id})
         tuple = self.c.fetchone()
-        igns = json.loads(tuple[14])
+        igns = json.loads(tuple[0])
         # Exit if no IGN exists
         if not igns:
-            await ctx.send(f'`{member}` did not add any IGN')
+            if oneself:
+                await ctx.reply('You have no IGN stored to show!')
+            else:
+                await ctx.reply(f'`{member}` has no IGN stored to show.')
             return
         if single:
             if game in igns:
-                await ctx.send(igns[game])
+                await ctx.reply(embed=discord.Embed(description=igns[game],color=member.top_role.color))
+            elif oneself:
+                await ctx.reply(f'You have no IGN stored for {game} to show!')
             else:
-                await ctx.send(f'`{member}` did not add any IGN for {game}')
+                await ctx.reply(f'`{member}` has no IGN stored for {game} to show.')
             return
         ign = ''
-        for key in igns:
-            ign += f'\n**{key}:** {igns[key]}'
+        for game in igns:
+            ign += f'\n**{game}:** {igns[game]}'
         embed = discord.Embed(
-            title = 'IGNs:',
+            title = f'{member}\'s IGNs:',
             description = ign,
             color = member.top_role.color
         )
-        embed.set_author(name = member, icon_url = member.avatar_url)
-        embed.set_thumbnail(url = member.avatar_url)
-        if ctx.author != member:
-            embed.set_footer(text = f'Requested by: {ctx.author}', icon_url = ctx.author.avatar_url)
-        await ctx.send(embed = embed)
+        embed.set_author(name=member, icon_url=member.avatar_url)
+        embed.set_thumbnail(url=member.avatar_url)
+        if not oneself:
+            embed.set_footer(text=f'Requested by: {ctx.author}', icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
 
-    @ign.command(name='delete')
-    async def delete(self, ctx):
+    @ign.command(name='delete', aliases=['del'])
+    async def delete(self, ctx, *args):
         # Gets details of user from the database
-        self.c.execute('SELECT * FROM main where Discord_UID = (:uid)', {'uid': ctx.author.id})
+        self.c.execute('SELECT IGN FROM main where Discord_UID = (:uid)', {'uid': ctx.author.id})
         tuple = self.c.fetchone()
-        igns = json.loads(tuple[14])
+        igns = json.loads(tuple[0])
         # Exit if no IGN exists
         if not igns:
-            await ctx.send(f'You do not any stored IGN to remove, {ctx.author.mention}.')
+            await ctx.reply('You have no IGN stored to remove.')
             return
-        try:
-            game = ctx.message.content.split('delete ')[1]
-        except:
+        if not args:
             # Remove all existing IGNs of the user from the database
             self.c.execute('UPDATE main SET IGN = "{}" where Discord_UID = (:uid)', {'uid': ctx.author.id})
             self.conn.commit()
-            await ctx.send(f'Removed all existing IGNs successfully, {ctx.author.mention}.')
+            await ctx.reply('Removed all existing IGNs successfully.')
             return
-        games = json.loads(os.getenv('Games'))
+        # Loads the available games from the database
+        games = self.data[str(ctx.guild.id)]
         # Checks if the game exists in the database
         if game not in games:
-            await ctx.send(f'The entered game does not exist in the database, {ctx.author.mention}. If you want it added, contact a moderator.\nFor a list of available games, type `{prefix}ign add`')
+            await ctx.reply(f'The entered game does not exist in the database. If you want it added, contact a moderator.\nFor a list of available games, type `{prefix}ign add`')
             return
-        igns = json.loads(tuple[14])
         if game in igns:
             igns.pop(game)
         else:
-            await ctx.send(f'You do not any stored IGN for {game}, {ctx.author.mention}.')
+            await ctx.reply(f'You have no IGN stored for {game} to remove.')
             return
         # Remove requested IGN of the user from the database
         self.c.execute('UPDATE main SET IGN = (:ign) where Discord_UID = (:uid)', {'ign': json.dumps(igns), 'uid': ctx.author.id})
         self.conn.commit()
-        await ctx.send(f'IGN for {game} removed successfully, {ctx.author.mention}.')
+        await ctx.reply(f'IGN for {game} removed successfully.')
