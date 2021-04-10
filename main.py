17793e656ce696db4fa71917012d0af34645b4e8
GetPsyched <priyanshutr@gmail.com>
2021-04-10 23:49:53 +0400

    Modified main.py
    - Moved all events from main.py to EventHandler.py
    
    - Restricted the tag feature to members with a verified email

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ from datetime import datetime, timedelta
 
 load_dotenv()
 
-sections = ['CE-A', 'CE-B', 'CE-C', 'CS-A', 'CS-B', 'EC-A', 'EC-B', 'EC-C', 'EE-A', 'EE-B', 'EE-C', 'IT-A', 'IT-B', 'ME-A', 'ME-B', 'ME-C', 'PI-A', 'PI-B']
-subsections = ['CE-01', 'CE-02', 'CE-03', 'CE-04', 'CE-05', 'CE-06', 'CE-07', 'CE-08', 'CE-09',
-            'CS-01', 'CS-02', 'CS-03', 'CS-04', 'CS-05', 'CS-06',
-            'EC-01', 'EC-02', 'EC-03', 'EC-04', 'EC-05', 'EC-06', 'EC-07', 'EC-08', 'EC-09',
-            'EE-01', 'EE-02', 'EE-03', 'EE-04', 'EE-05', 'EE-06', 'EE-07', 'EE-08', 'EE-09',
-            'IT-01', 'IT-02', 'IT-03', 'IT-04', 'IT-05', 'IT-06',
-            'ME-01', 'ME-02', 'ME-03', 'ME-04', 'ME-05', 'ME-06', 'ME-07', 'ME-08', 'ME-09',
-            'PI-01', 'PI-02', 'PI-03', 'PI-04', 'PI-05', 'PI-06'
-        ]
-
 def get_prefix(client, message):
     with open('db/guilds.json', 'r') as f:
         prefixes = json.load(f)
@@ -23,47 +13,6 @@ def get_prefix(client, message):
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=get_prefix, intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'Logged on as {client.user}!\n')
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'%help'))
-
-@client.event
-async def on_member_join(member):
-    # Exit if the user is a bot
-    if member.bot or member.guild.id == 336642139381301249:
-        return
-    conn = sqlite3.connect('db/details.db')
-    c = conn.cursor()
-    # Checks if the user who joined is already in the database or not
-    c.execute('SELECT * from main where Discord_UID = (:uid)', {'uid': member.id})
-    tuple = c.fetchone()
-    guild = member.guild
-    if tuple:
-        # Fetches the mutual guilds list from the user
-        guilds = json.loads(tuple[10])
-        # Adds the new guild id if it's a new one
-        if guild.id not in guilds:
-            guilds.append(guild.id)
-        guilds = json.dumps(guilds)
-        # Assigning one SubSection and one Section role to the user
-        role = get(guild.roles, name = tuple[2])
-        await member.add_roles(role)
-        role = get(guild.roles, name = tuple[3])
-        await member.add_roles(role)
-        # Updating the record in the database
-        c.execute('UPDATE main SET Guilds = (:guilds) where Discord_UID = (:uid)', {'uid': member.id, 'guilds': guilds})
-        conn.commit()
-        return
-    # Adding the 'Not-Verified' role if the user details do not exist in the database
-    role = get(guild.roles, name = 'Not-Verified')
-    await member.add_roles(role)
-    # Sends a dm to the new user explaining that they have to verify
-    dm_message = '''Welcome to the NITKKR'24 server!
-    Before you can see/use all the channels that it has, you'll need to do a quick verification. The process of which is explained in the #welcome channel of the server. Please do not send the command to this dm as it will not be read, instead send it on the #commands channel on the server. If you have any issues with the command, @Priyanshu will help you out personally on the channel. But do try even if you didn't understand.
-    Have fun!'''
-    await member.send(dm_message)
 
 @client.command(brief='Displays details of the user', aliases=['p'])
 async def profile(ctx):
@@ -116,6 +65,7 @@ async def memlist(ctx, batch: int=2024):
     """Displays the total number of members joined per section. Also displays the number of verified members on the server along with the total number of humans on the server"""
     conn = sqlite3.connect('db/details.db')
     c = conn.cursor()
+    sections = ['CE-A', 'CE-B', 'CE-C', 'CS-A', 'CS-B', 'EC-A', 'EC-B', 'EC-C', 'EE-A', 'EE-B', 'EE-C', 'IT-A', 'IT-B', 'ME-A', 'ME-B', 'ME-C', 'PI-A', 'PI-B']
     total = []
     joined = []
     verified = []
@@ -151,7 +101,7 @@ async def memlist(ctx, batch: int=2024):
     await ctx.send(embed=embed)
 
 @client.command(brief='Allows user to tag section/subsection roles')
-async def tag(ctx):
+async def tag(ctx, content):
     """**How to use:**
     Now you're able to tag roles of subsections _given_ that the said subsection falls in the same section that you are in.
     That means that if you're in IT-A, you can tag `IT-A, IT-01, IT-02, ...` but you can NOT tag `IT-B, IT-06, ME-B, CE-01, PI-06, ...`
@@ -164,12 +114,17 @@ async def tag(ctx):
     Capitalization does NOT matter.
 
     **PS:** If you're found to abuse this facility, ie. spam tags or tag people for an unimportant reason, then this facility will be revoked for you and you may be banned temporarily. Though I'll try to avoid such things. (And you should too)"""
-    # Assigns message content to variable
-    try:
-        msg = ctx.message.content.split('tag ')[1]
-    except:
-        await ctx.send(f'Type something after `{prefix}tag`')
-        return
+
+    sections = ['CE-A', 'CE-B', 'CE-C', 'CS-A', 'CS-B', 'EC-A', 'EC-B', 'EC-C', 'EE-A', 'EE-B', 'EE-C', 'IT-A', 'IT-B', 'ME-A', 'ME-B', 'ME-C', 'PI-A', 'PI-B']
+    subsections = ['CE-01', 'CE-02', 'CE-03', 'CE-04', 'CE-05', 'CE-06', 'CE-07', 'CE-08', 'CE-09',
+                'CS-01', 'CS-02', 'CS-03', 'CS-04', 'CS-05', 'CS-06',
+                'EC-01', 'EC-02', 'EC-03', 'EC-04', 'EC-05', 'EC-06', 'EC-07', 'EC-08', 'EC-09',
+                'EE-01', 'EE-02', 'EE-03', 'EE-04', 'EE-05', 'EE-06', 'EE-07', 'EE-08', 'EE-09',
+                'IT-01', 'IT-02', 'IT-03', 'IT-04', 'IT-05', 'IT-06',
+                'ME-01', 'ME-02', 'ME-03', 'ME-04', 'ME-05', 'ME-06', 'ME-07', 'ME-08', 'ME-09',
+                'PI-01', 'PI-02', 'PI-03', 'PI-04', 'PI-05', 'PI-06'
+            ]
+
     bool = False
     async with aiohttp.ClientSession() as session:
         # Checks if a webhook already exists for that channel
@@ -188,13 +143,13 @@ async def tag(ctx):
     conn = sqlite3.connect('db/details.db')
     c = conn.cursor()
     # Gets details of user from the database
-    c.execute('SELECT * FROM main where Discord_UID = (:uid)', {'uid': ctx.author.id})
+    c.execute('SELECT Verified, Section FROM main where Discord_UID = (:uid)', {'uid': ctx.author.id})
     tuple = c.fetchone()
-    if not tuple:
-        await ctx.send(f'Only verified members can be use this feature, {ctx.author.mention}.')
-        return
-    section = tuple[2]
-    for i in msg.split(' '):
+    if tuple[0] == 'False':
+        await ctx.reply('Only members with a verified email can use this command.')
+        raise Exception('Permission Denied (Absence of a verified email)')
+    section = tuple[1]
+    for i in content.split(' '):
         # Exit if the user tries to ping @everyone or @here
         if '@everyone' in i or '@here' in i:
             await ctx.send('You can\'t tag `@everyone` or `@here`')
@@ -212,7 +167,7 @@ async def tag(ctx):
                     usertag = True
             # Checks if a role has been tagged by its ID
             if '&' in j and int(j[1:-1]) in [role.id for role in ctx.guild.roles]:
-                msg = msg.replace('\<@' + j, '@' + ctx.guild.get_role(int(j[1:-1])).name)
+                content = content.replace('\<@' + j, '@' + ctx.guild.get_role(int(j[1:-1])).name)
                 j = j.replace(j, ctx.guild.get_role(int(j[1:-1])).name)
             # Skip to the next iteration if tagged section doesn't exist
             elif j[:4].upper() not in sections and j[:5].upper() not in subsections:
@@ -223,9 +178,9 @@ async def tag(ctx):
             # Checks if the user belongs to the tagged section
             if j and ctx.author.guild_permissions.mention_everyone:
                 if j[3] == '0':
-                    msg = msg.replace('@' + j[:5], discord.utils.get(ctx.guild.roles, name = j[:5].strip().upper()).mention)
+                    content = content.replace('@' + j[:5], discord.utils.get(ctx.guild.roles, name = j[:5].strip().upper()).mention)
                 elif j[3].upper() == section[3]:
-                    msg = msg.replace('@' + j[:4], discord.utils.get(ctx.guild.roles, name = j[:4].strip().upper()).mention)
+                    content = content.replace('@' + j[:4], discord.utils.get(ctx.guild.roles, name = j[:4].strip().upper()).mention)
             elif j and j[:2].upper() not in section:
                 await ctx.send('You can\'t tag sections other than your own!')
                 return
@@ -234,22 +189,22 @@ async def tag(ctx):
                 if j[3] == '0':
                     # Checks if the user belongs to the Section of the SubSection that they attempted to tag
                     if section[3] == 'A' and (j[4] == '1' or j[4] == '2' or j[4] == '3'):
-                        msg = msg.replace('@' + j[:5], discord.utils.get(ctx.guild.roles, name = j[:5].strip().upper()).mention)
+                        content = content.replace('@' + j[:5], discord.utils.get(ctx.guild.roles, name = j[:5].strip().upper()).mention)
                     elif section[3] == 'B' and (j[4] == '4' or j[4] == '5' or j[4] == '6'):
-                        msg = msg.replace('@' + j[:5], discord.utils.get(ctx.guild.roles, name = j[:5].strip().upper()).mention)
+                        content = content.replace('@' + j[:5], discord.utils.get(ctx.guild.roles, name = j[:5].strip().upper()).mention)
                     elif section[3] == 'C' and (j[4] == '7' or j[4] == '8' or j[4] == '9'):
-                        msg = msg.replace('@' + j[:5], discord.utils.get(ctx.guild.roles, name = j[:5].strip().upper()).mention)
+                        content = content.replace('@' + j[:5], discord.utils.get(ctx.guild.roles, name = j[:5].strip().upper()).mention)
                     else:
                         await ctx.send('You can\'t tag sections other than your own!')
                         return
                 elif j[3].upper() == section[3]:
-                    msg = msg.replace('@' + j[:4], discord.utils.get(ctx.guild.roles, name = j[:4].strip().upper()).mention)
+                    content = content.replace('@' + j[:4], discord.utils.get(ctx.guild.roles, name = j[:4].strip().upper()).mention)
                 else:
                     await ctx.send('You can\'t tag sections other than your own!')
                     return
     # Deletes the sent command and sends the new tagged version
     await ctx.message.delete()
-    await webhook.send(msg.strip(), username=username, avatar_url=ctx.author.avatar_url)
+    await webhook.send(content.strip(), username=username, avatar_url=ctx.author.avatar_url)
 
 @client.command(brief='Nicks the user to their first name')
 async def nick(ctx):
@@ -276,37 +231,6 @@ async def nick(ctx):
         await member.edit(nick = word[:1] + word[1:].lower())
         await ctx.send(f'Changed the nick of `{member}` successfully.')
 
-@client.event
-async def on_member_remove(member):
-    if member.bot or member.guild.id == 336642139381301249:
-        return
-    conn = sqlite3.connect('db/details.db')
-    c = conn.cursor()
-    # Gets details of user from the database
-    c.execute('SELECT * FROM main where Discord_UID = (:uid)', {'uid': member.id})
-    tuple = c.fetchone()
-    channel = client.get_channel(783215699707166763)
-    # Exit if the user was not found
-    if not tuple:
-        await channel.send(f'{member.mention} has left the server because they didn\'t know how to verify <a:triggered:803206114623619092>')
-        return
-    # Fetches the mutual guilds list from the user
-    guilds = json.loads(tuple[10])
-    # Removes the guild from the list
-    guilds.remove(member.guild.id)
-    # Remvoes their ID from the database if they don't have a verified email
-    # and this was the only guild they shared with the bot
-    if tuple[11] == 'False' and guilds:
-        guilds = json.dumps(guilds)
-        c.execute('UPDATE main SET Discord_UID = NULL, Guilds = (:guilds) where Discord_UID = (:uid)', {'uid': member.id, 'guilds': guilds})
-        conn.commit()
-    # Only removes the guild ID otherwise
-    else:
-        guilds = json.dumps(guilds)
-        c.execute('UPDATE main SET Guilds = (:guilds) where Discord_UID = (:uid)', {'uid': member.id, 'guilds': guilds})
-        conn.commit()
-    await channel.send(f'{member.mention} has left the server.')
-
 @client.command(brief='Gives invites of some servers', aliases=['inv'])
 async def invite(ctx):
     servers = ['NITKKR\'24: https://discord.gg/4eF7R6afqv',
@@ -320,50 +244,51 @@ async def invite(ctx):
     await ctx.send(embed=embed)
 
 @client.command(brief='Restarts the bot')
+@commands.has_permissions(manage_guild=True)
 async def restart(ctx):
     """Restarts the bot. Can only be used by members with the `Manage Server` permission."""
-    if not ctx.author.guild_permissions.manage_guild:
-        await ctx.send('This command requires you to have the `Manage Server` permission to use it')
-        return
     await ctx.message.delete()
     await client.close()
 
-@client.event
-async def on_voice_state_update(member, before, after):
-    if member.bot:
-        return
-    if before.channel and before.channel != after.channel:
+async def reminder_loop():
+    await client.wait_until_ready()
+    while not client.is_closed():
         try:
-            with open('db/VCs.json') as f:
+            with open('db/reminders.json') as f:
                 data = json.load(f)
         except FileNotFoundError:
-            data = []
-        if before.channel.id in data:
-            if not len(before.channel.members):
-                await before.channel.delete()
-                data.remove(before.channel.id)
-            with open('db/VCs.json', 'w') as f:
-                json.dump(data, f)
-            return
-    if not after.channel:
-        return
-    if after.channel.id not in [825422681695846430, 825456619211063327]:
-        return
-    if member.nick:
-        member_name = member.nick
-    else:
-        member_name = member.name
-    vc = await member.guild.create_voice_channel(f'{member_name}\'s Party', category=after.channel.category)
-    await member.move_to(vc)
-    try:
-        with open('db/VCs.json') as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = []
-    if vc.id not in data:
-        data.append(vc.id)
-        with open('db/VCs.json', 'w') as f:
-            json.dump(data, f)
+            data = {}
+        IST = pytz.timezone('Asia/Kolkata')
+        datetime_ist = datetime.now(IST)
+        for i in data.copy():
+            time = IST.localize(datetime.strptime(data[i]['time'], '%Y-%m-%d %H:%M:%S'))
+            if time <= datetime_ist:
+                embed = discord.Embed(
+                    title = 'Reminder',
+                    description = message,
+                    color = discord.Color.blurple()
+                )
+                channel = client.get_channel(data[i]['channel'])
+                if channel:
+                    await channel.send(embed=embed)
+                else:
+                    author = client.get_user(data[i]['author'])
+                    await author.send(embed=embed)
+                if 'False' not in data[i]['repeat']:
+                    if data[i]['repeat'] == 'daily':
+                        time += timedelta(days=1)
+                    elif data[i]['repeat'] == 'weekly':
+                        time += timedelta(days=7)
+                    elif data[i]['repeat'] == 'monthly':
+                        time += timedelta(months=1)
+                    elif data[i]['repeat'] == 'yearly':
+                        time += timedelta(years=1)
+                    data[i]['time'] = time.strftime('%Y-%m-%d %H:%M:%S')
+                else:
+                    del data[i]
+                with open('db/reminders.json', 'w') as f:
+                    json.dump(data, f)
+        await asyncio.sleep(1)
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
