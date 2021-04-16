7cbcbf608c84166f05de69f0b48741bf3a26e627
GetPsyched <priyanshutr@gmail.com>
2021-04-16 15:09:15 +0400

    Added UserInfo Cog
    - Moved profile() and nick() to userinfo.py from main.py
    
    - The 'Roles' field in the profile embed created by profile() now shows 
    the roles in the order as shown on the user's profile.
    
    - Added support for the nick() command to be usable by anyone. This 
    means 2 things:
        • Any member can change their own nick to their first name by using 
    the command without any arguments.
        • Members who either have one of the specified mod roles for the 
    server, manage nicknames permission, or are the server owner can use 
    this command to change the nicknames of other members of the guild.
    
    - General performance improvements to profile()

---
+++ cogs/userinfo.py
@@ -0,0 +1,117 @@
+import discord, json, sqlite3
+from datetime import datetime
+from discord.ext import commands
+
+class UserInfo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        try:
+            with open('db/guilds.json', 'r') as f:
+                self.data = json.load(f)
+        except FileNotFoundError:
+            self.bot.loop.create_task(self.create())
+        with open('db/emojis.json', 'r') as f:
+            self.emojis = json.load(f)
+
+        self.conn = sqlite3.connect('db/details.db')
+        self.c = self.conn.cursor()
+
+    @commands.command(name='profile', brief='Displays details of the user', aliases=['p'])
+    async def profile(self, ctx, member: discord.Member=None):
+        """Displays details of the user related to the server and the college"""
+        member = member or ctx.author
+        if not await self.check(ctx, member):
+            return
+        # Gets details of requested user from the database
+        self.c.execute('SELECT Roll_Number, Section, SubSection, Name, Institute_Email, Verified FROM main where Discord_UID = (:uid)', {'uid': member.id})
+        tuple = self.c.fetchone()
+        # Exit if the user was not found
+        if not tuple:
+            await ctx.reply('The requested record wasn\'t found!')
+            return
+        # Creates a list of role objects of the user to display in the embed
+        ignored_roles = [tuple[1], tuple[2], '@everyone']
+        user_roles = [role.mention for role in member.roles if role.name not in ignored_roles]
+        user_roles.reverse()
+        user_roles = ', '.join(user_roles)
+        if not user_roles:
+            user_roles = 'None taken'
+        # Checking if the user has a verified email or not
+        status = ' '
+        if tuple[5] == 'True':
+            status += self.emojis['verified']
+        else:
+            status += self.emojis['not-verified']
+        # Creating the embed
+        embed = discord.Embed(
+            title = ' '.join([word[:1] + word[1:].lower() for word in tuple[3].split(' ')]) + status,
+            description = f'**Roll Number:** {tuple[0]}'
+            + f'\n**Section:** {tuple[1]}{tuple[2][4:]}'
+            + f'\n**Roles:** {user_roles}'
+            + f'\n**Email:** {tuple[4]}',
+            colour = member.top_role.color
+        )
+        embed.set_author(name = f'{member}\'s Profile', icon_url = member.avatar_url)
+        embed.set_thumbnail(url = member.avatar_url)
+        join_date = member.joined_at.strftime('%b %d, %Y')
+        embed.set_footer(text = f'Joined at: {join_date}')
+        await ctx.send(embed=embed)
+
+    @commands.command(name='nick', brief='Nicks a user to their first name')
+    @commands.has_permissions(change_nickname=True)
+    async def nick(self, ctx, member: discord.Member=None):
+        """Changes name of the user to their first name as in the database. Can only be used by members with the `Manage Nicknames` permission."""
+        member = member or ctx.author
+        if not await self.check(ctx, member):
+            return
+        self.c.execute('SELECT Name FROM main where Discord_UID = (:uid)', {'uid': member.id})
+        tuple = self.c.fetchone()
+        # Exit if the user was not found
+        if not tuple:
+            await ctx.reply(f'`{member}` does not exist in the database')
+            return
+        old_nick = member.nick
+        word = tuple[0].split(' ')[0]
+        await member.edit(nick = word[:1] + word[1:].lower())
+        await ctx.reply(f'Changed the nick of `{member}` from `{old_nick}` to `{member.nick}` successfully.')
+
+    async def check(self, ctx, member):
+        if member != ctx.author:
+            if ctx.author.guild_permissions.manage_nicknames:
+                return True
+            # Fetches the moderator roles set for that guild
+            if mod_roles := [ctx.guild.get_role(role) for role in self.data[str(ctx.guild.id)]['mod_roles']]:
+                flag = False
+                for mod_role in mod_roles:
+                    if mod_role in ctx.author.roles:
+                        flag = True
+                        break
+                # Exit if the author is not a moderator
+                if not flag:
+                    await ctx.reply('You\'re not authorised to use this command.')
+                    return False
+            else:
+                await ctx.send('No moderator role has been set for this guild. Set moderator roles using the `setmod` command.')
+                return False
+        return True
+
+    async def create(self):
+        await self.bot.wait_until_ready()
+        default_details = {
+            'prefix': ['%'],
+            'mod_roles': [],
+            'verification': False,
+            'join_msg_channel': 0,
+            'leave_msg_channel': 0,
+            'logging_channel': [0, 0]
+        }
+        self.data = dict([(guild.id, default_details) for guild in self.bot.guilds])
+        self.save()
+
+    def save(self):
+        with open('db/guilds.json', 'w') as f:
+            json.dump(self.data, f)
+
+def setup(bot):
+    bot.add_cog(UserInfo(bot))
--- main.py
+++ main.py
@@ -13,67 +13,6 @@ def get_prefix(client, message):
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=get_prefix, intents=intents)
 
-@client.command(brief='Displays details of the user', aliases=['p'])
-async def profile(ctx):
-    """Displays details of the user related to the server and the college"""
-    member = ctx.message.mentions
-    if member and member[0] != ctx.author:
-        try:
-            with open('db/guilds.json', 'r') as f:
-                data = json.load(f)
-        except FileNotFoundError:
-            data = {str(ctx.guild.id): {'prefix': ['%'], 'mod_role': []}}
-            with open('db/guilds.json', 'w') as f:
-                json.dump(data, f)
-        # Fetches the moderator roles set for that guild
-        if mod_roles := [ctx.guild.get_role(role) for role in data[str(ctx.guild.id)]['mod_roles']]:
-            flag = False
-            for mod_role in mod_roles:
-                if mod_role in ctx.author.roles:
-                    flag = True
-                    break
-            # Exit if the author is not a moderator
-            if not flag:
-                await ctx.reply('You\'re not authorised to use this command.')
-                return
-        else:
-            await ctx.send('No moderator role has been set for this guild. Set moderator roles using the `setmod` command.')
-            return
-        member = member[0]
-    else:
-        member = ctx.author
-    conn = sqlite3.connect('db/details.db')
-    c = conn.cursor()
-    # Gets details of requested user from the database
-    c.execute('SELECT * FROM main where Discord_UID = (:uid)', {'uid': member.id})
-    tuple = c.fetchone()
-    # Exit if the user was not found
-    if not tuple:
-        await ctx.send('The requested record wasn\'t found!')
-        return
-    # Creates a list of role objects of the user to display in the embed
-    roles = ', '.join([role.mention for role in member.roles if tuple[2] != role.name and tuple[3] != role.name and '@everyone' != role.name])
-    if not roles:
-        roles = 'None taken'
-    # Checking if the user has a verified email or not
-    if tuple[11] == 'True':
-        verification_status = ' <:verified:819460140247810059>'
-    else:
-        verification_status = ' <:notverified:819460105250537483>'
-    # Creating the embed
-    embed = discord.Embed(
-        title = ' '.join([word[:1] + word[1:].lower() for word in tuple[4].split(' ')]) + verification_status,
-        description = '**Roll Number: **' + str(tuple[1])
-        + '\n**Section: **' + tuple[2] + tuple[3][4:]
-        + '\n**Roles: **' + roles
-        + '\n**Email: **' + tuple[6],
-        colour = member.top_role.color
-    )
-    embed.set_author(name = str(member) + '\'s Profile', icon_url = member.avatar_url)
-    embed.set_thumbnail(url = member.avatar_url)
-    embed.set_footer(text = 'Joined at: ' + str(member.joined_at)[8:10] + '-' + str(member.joined_at)[5:7] + '-' + str(member.joined_at)[:4])
-    await ctx.send(embed = embed)
-
 @client.command(brief='Segregated display of the number of members')
 async def memlist(ctx, batch: int=2024):
     """Displays the total number of members joined per section. Also displays the number of verified members on the server along with the total number of humans on the server"""
@@ -113,28 +52,6 @@ async def memlist(ctx, batch: int=2024):
         color = discord.Color.blurple()
     )
     await ctx.send(embed=embed)
-
-@client.command(brief='Nicks the user to their first name')
-@commands.has_permissions(manage_nicknames=True)
-async def nick(ctx):
-    """Changes name of the user to their first name as in the database. Can only be used by members with the `Manage Nicknames` permission."""
-    # Exit if no one was tagged
-    if not ctx.message.mentions:
-        await ctx.reply('Tag someone to reset their nickname.')
-        return
-    conn = sqlite3.connect('db/details.db')
-    c = conn.cursor()
-    for member in ctx.message.mentions:
-        # Gets details of user from the database
-        c.execute('SELECT Name FROM main where Discord_UID = (:uid)', {'uid': member.id})
-        tuple = c.fetchone()
-        # Exit if the user was not found
-        if not tuple:
-            await ctx.send(f'{member} does not exist in the database')
-            return
-        word = tuple[0].split(' ')[0]
-        await member.edit(nick = word[:1] + word[1:].lower())
-        await ctx.send(f'Changed the nick of `{member}` successfully.')
 
 @client.command(brief='Gives invites of some servers', aliases=['inv'])
 async def invite(ctx):
