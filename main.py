bd85f29067a5027b0ac616ec1e4da01792a86a47
GetPsyched <priyanshutr@gmail.com>
2021-04-18 15:32:36 +0400

    Added backwards compatibility to older versions of guilds.json
    - If new keys are added to guilds.json which don't exist in the file 
    yet, it now adds those missing keys.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import discord, os, sqlite3, json, asyncio
-from discord.utils import get
+import discord, os, sqlite3, json
 from discord.ext import commands
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,25 +12,30 @@ def get_prefix(client, message):
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=get_prefix, intents=intents)
+client.launch_time = datetime.utcnow()
 
 @client.event
 async def on_ready():
     print(f'Logged on as {client.user}!\n')
     await client.change_presence(activity=discord.Game(f'@{client.user.name}'))
 
-    # Creates the guilds.json file if it doesn't exist as it is essential for many cog's functioning
+    default_details = {
+        'prefix': ['%'],
+        'mod_roles': [],
+        'bot_role': 0,
+        'logging_channel': [0, 0]
+    }
     try:
-        with open('db/guilds.json') as f:
-            _ = json.load(f)
+        with open('db/guilds.json', 'r') as f:
+            details = json.load(f)
+        for guild in details:
+            for key in default_details:
+                if key not in details[guild]:
+                    details[guild][key] = default_details[key]
+        with open('db/guilds.json', 'w') as f:
+            json.dump(details, f)
     except FileNotFoundError:
-        default_details = {
-            'prefix': ['%'],
-            'mod_roles': [],
-            'verification': False,
-            'join_msg_channel': 0,
-            'leave_msg_channel': 0,
-            'logging_channel': [0, 0]
-        }
+        # Creates the guilds.json file if it doesn't exist as it is essential for many cog's functioning
         data = dict([(guild.id, default_details) for guild in client.guilds])
         with open('db/guilds.json', 'w') as f:
             json.dump(data, f)
