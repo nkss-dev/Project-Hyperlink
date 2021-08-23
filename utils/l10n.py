3e85f8f745cbb29bcfca92907406af9614600da7
GetPsyched <priyanshutr@gmail.com>
2021-08-23 10:50:19 +0400

    Cache l10n

--- utils/l10n.py
+++ utils/l10n.py
@@ -3,9 +3,15 @@ from fluent.runtime import FluentLocalization, FluentResourceLoader
 
 loader = FluentResourceLoader("l10n/{locale}")
 
+loaded = {}
 def get_l10n(guild_id, filename):
-    with open('db/guilds.json') as f:
-        guild_data = json.load(f)
+    if l10n := loaded.get(guild_id):
+        return l10n
+    else:
+        with open('db/guilds.json') as f:
+            guild_data = json.load(f)
 
-    language = guild_data[str(guild_id)].get('language', 'en-gb')
-    return FluentLocalization([language], [f'{filename}.ftl'], loader)
+        language = guild_data[str(guild_id)].get('language', 'en-gb')
+        loaded[guild_id] = FluentLocalization([language], [f'{filename}.ftl'], loader)
+
+        return loaded[guild_id]
