980f958fd89707e7b24489a0cea5aa8d9ac8b46a
GetPsyched <priyanshutr@gmail.com>
2021-09-02 11:46:39 +0400

    Optimise l10n
    - Change loaded dict structure to avoid duplicate FluentLocalization 
    objects
    - Add support for DMs

--- utils/l10n.py
+++ utils/l10n.py
@@ -4,17 +4,16 @@ from fluent.runtime import FluentLocalization, FluentResourceLoader
 loader = FluentResourceLoader("l10n/{locale}")
 
 loaded = {}
-def get_l10n(guild_id, filename):
-    if (l10n := loaded.get(guild_id)) and (l10n := l10n.get(filename)):
-        return l10n
-
+def get_l10n(id, filename):
     with open('db/guilds.json') as f:
         guild_data = json.load(f)
+    language = guild_data[str(id)].get('language', 'en-gb') if id else 'en-gb'
 
-    if not loaded.get(guild_id):
-        loaded[guild_id] = {}
+    if (l10n := loaded.get(language)) and (l10n := l10n.get(filename)):
+        return l10n
 
-    language = guild_data[str(guild_id)].get('language', 'en-gb')
-    loaded[guild_id][filename] = FluentLocalization([language], [f'{filename}.ftl'], loader)
+    if not loaded.get(language):
+        loaded[language] = {}
 
-    return loaded[guild_id][filename]
+    loaded[language][filename] = FluentLocalization([language], [f'{filename}.ftl'], loader)
+    return loaded[language][filename]
