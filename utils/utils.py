704edc5c5eff79d4b9084165cbbb4e0209be7814
GetPsyched <priyanshutr@gmail.com>
2021-09-17 19:55:26 +0400

    Add function to handle yes/no questions

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,12 @@
-async def deleteOnReaction(ctx, message, emoji: str='🗑️'):
-    """deletes the given message when a certain reaction is used"""
+import discord
+import json
+
+async def deleteOnReaction(ctx, message: discord.Message, emoji: str='🗑️'):
+    """Delete the given message when a certain reaction is used"""
     await message.add_reaction(emoji)
 
     def check(reaction, member):
-        if reaction.emoji != emoji or member == ctx.bot.user:
+        if str(reaction.emoji) != emoji or member == ctx.bot.user:
             return False
         if member != ctx.author and not member.guild_permissions.manage_messages:
             return False
@@ -16,7 +19,8 @@ async def deleteOnReaction(ctx, message, emoji: str='🗑️'):
     if ctx.guild and ctx.guild.me.guild_permissions.manage_messages:
         await ctx.message.delete()
 
-async def getWebhook(channel, member):
+async def getWebhook(channel: discord.TextChannel, member: discord.Member):
+    """Return a webhook"""
     for webhook in await channel.webhooks():
         if webhook.user == member:
             return webhook
@@ -26,3 +30,24 @@ async def getWebhook(channel, member):
             avatar=await member.display_avatar.read()
         )
         return webhook
+
+async def yesOrNo(ctx, message: discord.Message) -> bool:
+    """Return true or false based on the user's reaction"""
+    with open('db/emojis.json') as f:
+        emojis = json.load(f)['utility']
+
+    reactions = (emojis['yes'], emojis['no'])
+
+    for reaction in reactions:
+        await message.add_reaction(reaction)
+
+    def check(reaction, member):
+        if str(reaction.emoji) not in reactions:
+            return False
+        if member == ctx.bot.user or member != ctx.author or reaction.message != message:
+            return False
+        return True
+
+    reaction, _ = await ctx.bot.wait_for('reaction_add', check=check)
+    await message.delete()
+    return str(reaction.emoji) == reactions[0]
