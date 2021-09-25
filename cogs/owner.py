a20a9f369660d507857d1ee3dba2ed0c6cede540
GetPsyched <priyanshutr@gmail.com>
2021-09-25 23:38:52 +0400

    Add commands to manage db files

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,7 +1,13 @@
 import json
 from utils.l10n import get_l10n
+from utils.utils import yesOrNo
 
+import discord
 from discord.ext import commands
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class OwnerOnly(commands.Cog):
     """Bot owner commands"""
@@ -70,6 +76,90 @@ class OwnerOnly(commands.Cog):
 
         await ctx.message.remove_reaction(self.emojis['loading'], self.bot.user)
 
+    @commands.group(aliases=['db'], invoke_without_command=True)
+    async def database(self, ctx):
+        """Command group for database read/write commands"""
+        await ctx.send_help(ctx.command)
+
+    @database.command(aliases=['arc', 'arch'])
+    async def archive(self, ctx, *filenames):
+        """Create a save state of the database files.
+
+        Paramters
+        -----------
+        `filenames`: Optional[list[str]]
+            A list of file names of the files to archive. If not specified, all \
+            files in the database folder will be archived.
+        """
+        if isinstance(ctx.channel, discord.TextChannel):
+            members = [member for member in ctx.channel.members if not member.bot]
+            if len(members) > 1:
+                message = await ctx.reply(self.l10n.format_value('reveal-check'))
+                if not await yesOrNo(ctx, message):
+                    await ctx.send(self.l10n.format_value('archive-cancel'))
+                    return
+
+        files = []
+        if filenames:
+            for filename in filenames:
+                try:
+                    files.append(discord.File(f'db/{filename}'))
+                except FileNotFoundError:
+                    pass
+        else:
+            for i, filename in enumerate(os.listdir('./db'), start=1):
+                files.append(discord.File(f'db/{filename}'))
+
+        # Dividing the files into lists of 10 as the
+        # max. limit of attachments per message is 10
+        files = [files[i:i+10] for i in range(0, len(files), 10)]
+
+        if not files:
+            await ctx.send(self.l10n.format_value('file-notfound'))
+        for ten_files in files:
+            await ctx.send(files=ten_files)
+
+    @database.command()
+    async def add(self, ctx, *, filenames):
+        """Add file(s) to the database folder.
+
+        Paramters
+        -----------
+        `filenames`: <class 'str'>
+            A comma-separated string of the file names of the files to add.
+            If there are more file names than files, the remaining file names \
+            will be discarded. If left blank, the file names will default to \
+            the attachment names. To leave certain file names blank, enter two \
+            or more consecutive commas; hence, leaving those files' name blank.
+        """
+        if not ctx.message.attachments:
+            await ctx.reply(self.l10n.format_value('attachment-notfound'))
+            return
+
+        if filenames:
+            filenames = filenames.split(',')
+        else:
+            filenames = []
+        if diff := len(ctx.message.attachments) - len(filenames) > 0:
+            filenames.extend([None]*diff)
+        for filename, attachment in zip(filenames, ctx.message.attachments):
+            await attachment.save(f'db/{filename or attachment.filename}')
+
+        await ctx.send(self.l10n.format_value('upload-success'))
+
+    @database.command(aliases=['rm'])
+    async def remove(self, ctx, *filenames):
+        """Remove file(s) from the database folder.
+
+        Paramters
+        -----------
+        `filenames`: <class 'list'>
+            The list of file names to remove.
+        """
+        for filename in filenames:
+            os.remove(f'db/{filename}')
+
+        await ctx.send(self.l10n.format_value('remove-success'))
 
 def setup(bot):
     """Called when this file is attempted to be loaded as an extension"""
--- l10n/en-gb/owner.ftl
+++ l10n/en-gb/owner.ftl
@@ -1,5 +1,12 @@
 load-successful = 'cogs.{$ext}' loaded successfully!
-
+unload-successful = 'cogs.{$ext}' unloaded successfully!
 reload-successful = 'cogs.{$ext}' reloaded successfully!
 
-unload-successful = 'cogs.{$ext}' unloaded successfully!
+archive-cancel = Archive cancelled
+file-notfound = No file was found!
+reveal-check = Do you want to reveal the database file(s) in this channel with members other than you?
+
+attachment-notfound = Please add an attachment to your message and try again.
+upload-success = The file(s) have been uploaded successfully
+
+remove-success = The file(s) have been removed successfully
