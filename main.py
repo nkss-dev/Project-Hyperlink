b9b317036bd37ac58e9dc95a8e3b831188a037a4
GetPsyched <priyanshutr@gmail.com>
2021-10-25 13:59:58 +0400

    Use inherited class for the bot

--- main.py
+++ main.py
@@ -1,34 +1,59 @@
 import config
 import re
 
-from discord import Intents
+import discord
 from discord.ext import commands
+
 from utils.constructor import Constructor
 
 
-def get_prefix(_, message) -> str:
-    """return the bot's prefix for a guild or a DM"""
-    if message.guild:
-        return client.guild_data[str(message.guild.id)]['prefix']
-    return '%'
+class ProjectHyperlink(commands.Bot):
+    """A Discord bot for servers based around NITKKR"""
+
+    def __init__(self):
+        intents = discord.Intents(
+            guilds=True,
+            members=True,
+            bans=True,
+            emojis=True,
+            voice_states=True,
+            messages=True,
+            reactions=True
+        )
+        super().__init__(
+            command_prefix=self._prefix_callable,
+            intents=intents,
+            owner_ids=config.owner_ids
+        )
+
+        self.loop.create_task(self.construct())
+
+    @staticmethod
+    def _prefix_callable(bot, msg) -> list:
+        """Return the bot's prefix for a guild or a DM"""
+        base = []
+        if not msg.guild:
+            base.append('%')
+        else:
+            base.extend(bot.guild_data[str(msg.guild.id)]['prefix'])
+        return base
+
+    async def construct(self):
+        """Setup all initial requirements"""
+        await self.wait_until_ready()
+        Constructor(self)
+
+    def run(self):
+        super().run(config.bot_token, reconnect=True)
 
 
-client = commands.Bot(
-    command_prefix=get_prefix,
-    intents=Intents.all(),
-    owner_ids=config.owner_ids
-)
+if __name__ == '__main__':
+    bot = ProjectHyperlink()
 
+    @bot.before_invoke
+    async def bracketCheck(ctx):
+        """Raise an error if any argument is enclosed in angular brackets"""
+        if re.search(r'<[^#@].+>', ctx.message.content):
+            raise commands.CheckFailure('AngularBracketsNotAllowed')
 
-@client.event
-async def on_ready():
-    """invoked when the bot logs in successfully"""
-    Constructor(client)
-
-
-@client.before_invoke
-async def bracketCheck(ctx):
-    if re.search(r'<[^#@].+>', ctx.message.content):
-        raise commands.CheckFailure('AngularBracketsNotAllowed')
-
-client.run(config.bot_token)
+    bot.run()
