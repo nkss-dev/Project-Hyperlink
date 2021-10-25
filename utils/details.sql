6c143d704452e6d7ce934519fa52348311a3314a
GetPsyched <priyanshutr@gmail.com>
2021-10-25 16:53:58 +0400

    Add tables for clubs and societies

--- utils/details.sql
+++ utils/details.sql
@@ -1,4 +1,4 @@
-create table if not exists hostel (
+create table if not exists hostels (
     Hostel_Number  text primary key check(Hostel_Number like '%H_'),
     Hostel_Name    text unique,
     Warden_Name    text unique
@@ -8,21 +8,39 @@ create table if not exists main (
     Roll_Number      int  primary key,
     Section          text check(Section like '__-_'),
     SubSection       text check(SubSection like '__-0_'),
-    Name             text,
+    Name             text not null,
     Gender           text check(Gender='Male' or Gender='Female' or Gender='Other'),
     Mobile           text unique,
     Birthday         date,
     Institute_Email  text check(Institute_Email like '%___@___%.__%'),
-    Batch            int  check(length(Batch)=4),
-    Hostel_Number    text,
+    Batch            int  check(Batch >= 2010),
+    Hostel_Number    text references hostels(Hostel_Number),
     Room_Number      text check(Room_Number like '%_-___') unique,
     Discord_UID      int  unique,
-    Verified         text check(Verified='True' or Verified='False') default 'False',
-    foreign key(Hostel_Number) references hostel(Hostel_Number)
+    Verified         boolean default false
+);
+
+create table if not exists groups (
+    Name             text primary key,
+    Faculty_Advisor  text,
+    Contact_Number   text,
+    Kind             text check(Kind='Cultural Club' or Kind='Technical Society'),
+    Discord_Server   int,
+    Fresher_Role     int,
+    Sophomore_Role   int,
+    Junior_Role      int,
+    Final_Role       int,
+    Guest_Role       int
+);
+
+create table if not exists group_members (
+    Roll_Number  int  references main(Roll_Number),
+    Group_Name   text references groups(Name),
+    primary key(Roll_Number, Group_Name)
 );
 
 create table if not exists ign (
-    `Discord_UID`     int primary key not null,
+    `Discord_UID`     int primary key,
     `Chess`           text,
     `Clash of Clans`  text,
     `Clash Royale`    text,
@@ -37,7 +55,7 @@ create table if not exists ign (
     `PUBG`            text,
     `Rocket League`   text,
     `Valorant`        text,
-    foreign key(Discord_UID) references main(Discord_UID)
+    foreign key(Discord_UID) references main(Discord_UID) on update cascade
 );
 
 create table if not exists voltorb (
