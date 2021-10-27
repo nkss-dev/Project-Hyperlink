10ab7987560eb778dccb41183e0280fc729e4ed4
GetPsyched <priyanshutr@gmail.com>
2021-10-27 23:36:33 +0400

    Update get_l10n to get from sql

--- utils/l10n.py
+++ utils/l10n.py
@@ -1,15 +1,20 @@
-import json
+import sqlite3
 from fluent.runtime import FluentLocalization, FluentResourceLoader
 
 loader = FluentResourceLoader("l10n/{locale}")
-
 loaded = {}
-def get_l10n(id, filename):
-    with open('db/guilds.json') as f:
-        guild_data = json.load(f)
-    language = guild_data[str(id)].get('language', 'en-gb') if id else 'en-gb'
 
-    if (l10n := loaded.get(language)) and (l10n := l10n.get(filename)):
+conn = sqlite3.connect('db/details.db')
+c = conn.cursor()
+
+
+def get_l10n(id, filename) -> FluentLocalization:
+    language = c.execute(
+        'select Language from guilds where ID = ?', (id,)
+    ).fetchone()
+    language = language[0] if language else 'en-gb'
+
+    if (lang := loaded.get(language)) and (l10n := lang.get(filename)):
         return l10n
 
     if not loaded.get(language):
