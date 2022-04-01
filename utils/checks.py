ab0a4d8e997dd6bc9c23557e342e965f6e60c98a
GetPsyched <priyanshutr@gmail.com>
2022-04-02 01:36:31 +0530

    [checks] Add compatibility with PostgreSQL

--- utils/checks.py
+++ utils/checks.py
@@ -6,10 +6,19 @@ from discord.ext import commands
 def is_exists():
     """Check if the user has completed basic verification"""
     async def pred(ctx):
-        verified = ctx.bot.c.execute(
-            'select exists (select Verified from main where Discord_UID = ?)',
-            (ctx.author.id,)
-        ).fetchone()
+        verified = await ctx.bot.conn.fetchval(
+            '''
+            SELECT
+                exists (
+                    SELECT
+                        verified
+                    FROM
+                        student
+                    WHERE
+                        discord_uid = $1
+                )
+            ''', ctx.author.id
+        )
 
         if not verified:
             raise commands.CheckFailure('AccountNotLinked')
@@ -20,13 +29,14 @@ def is_exists():
 def is_verified():
     """Check if the user's identity has been verified"""
     async def pred(ctx):
-        verified = ctx.bot.c.execute(
-            'select Verified from main where Discord_UID = ?', (ctx.author.id,)
-        ).fetchall()
+        verified = await ctx.bot.conn.fetch(
+            'SELECT verified FROM student WHERE discord_uid = $1',
+            ctx.author.id
+        )
 
         if not verified:
             raise commands.CheckFailure('AccountNotLinked')
-        if not verified[0]:
+        if not verified[0]['verified']:
             raise commands.CheckFailure('UserNotVerified')
         return True
     return commands.check(pred)
@@ -44,10 +54,11 @@ def is_mod():
             return True
 
         # Fetches the moderator roles set for the guild
-        mod_roles = ctx.bot.c.execute(
-            'select role from mod_roles where ID = ?', (ctx.guild.id,)
-        ).fetchall()
+        mod_roles = await ctx.bot.conn.fetch(
+            'SELECT role FROM mod_role WHERE id = $1', ctx.guild.id
+        )
         if mod_roles:
+            mod_roles = [role['role'] for role in mod_roles]
             await commands.has_any_role(*mod_roles).predicate(ctx)
         else:
             raise commands.CheckFailure('MissingModeratorRoles')
