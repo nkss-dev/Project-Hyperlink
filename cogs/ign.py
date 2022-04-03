52687a6ba9ee7848f1909a6ccc4986f400c0ebd4
GetPsyched <priyanshutr@gmail.com>
2022-04-03 09:19:44 +0530

    Depreciate IGN
    Table structure needs to be updated along with majority of the code's internal functionality

--- cogs/ign.py
+++ cogs/ign.py
@@ -9,7 +9,7 @@ from utils.l10n import get_l10n
 
 
 class IGN(commands.Cog):
-    """Store IGNs for games"""
+    """[depreciated] Store IGNs for games"""
 
     def __init__(self, bot):
         self.bot = bot
