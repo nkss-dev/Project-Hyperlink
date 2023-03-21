297b3029c68f3e4ab9a240543e0bdc70cefd7ac3
GetPsyched <priyanshutr@proton.me>
2023-03-21 12:41:43 +0530

    Add sync command

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Literal, Optional
 
 import discord
 from discord.ext import commands
@@ -157,6 +158,43 @@ class OwnerOnly(commands.Cog):
             os.remove(f'db/{filename}')
 
         await ctx.send(self.fmv('remove-success'))
+
+    @commands.command()
+    @commands.guild_only()
+    async def sync(
+        self,
+        ctx: commands.Context,
+        guilds: commands.Greedy[discord.Object],
+        spec: Optional[Literal["~", "*", "^"]] = None,
+    ) -> None:
+        if not guilds:
+            if spec == "~":
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "*":
+                ctx.bot.tree.copy_global_to(guild=ctx.guild)
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            elif spec == "^":
+                ctx.bot.tree.clear_commands(guild=ctx.guild)
+                await ctx.bot.tree.sync(guild=ctx.guild)
+                synced = []
+            else:
+                synced = await ctx.bot.tree.sync()
+
+            await ctx.send(
+                f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+            )
+            return
+
+        ret = 0
+        for guild in guilds:
+            try:
+                await ctx.bot.tree.sync(guild=guild)
+            except discord.HTTPException:
+                pass
+            else:
+                ret += 1
+
+        await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
 
 
 async def setup(bot):
