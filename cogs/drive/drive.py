056611f7f4fb81c09c8fdfb5c0520970938c137e
zen0-5338 <shahjeet767@gmail.com>
2023-05-11 18:00:59 +0530

    use enum for exam choices

--- cogs/drive/drive.py
+++ cogs/drive/drive.py
@@ -1,9 +1,18 @@
+from enum import Enum
+
 import discord
 from discord.app_commands import Choice, Group
 
 from base.cog import HyperlinkGroupCog
 from cogs.drive.ui import DriveSearchView
 from main import ProjectHyperlink
+
+
+class ExamChoice(Enum):
+    MID_SEM_1 = 1
+    MID_SEM_2 = 2
+    MID_SEM_3 = 3
+    END_SEM = 4
 
 
 class Drive(
@@ -31,17 +40,18 @@ class Drive(
     @discord.app_commands.command(description="Upload a past paper.")
     @discord.app_commands.choices(
         exam=[  # TODO: probably use an enum for the choices
-            Choice(name="Mid Sem - 1", value="ms1"),
-            Choice(name="Mid Sem - 2", value="ms2"),
-            Choice(name="Mid Sem - 3", value="ms3"),
-            Choice(name="End Sem", value="es"),
+            Choice(name="Mid Sem - 1", value=ExamChoice.MID_SEM_1.value),
+            Choice(name="Mid Sem - 2", value=ExamChoice.MID_SEM_2.value),
+            Choice(name="Mid Sem - 3", value=ExamChoice.MID_SEM_3.value),
+            Choice(name="End Sem", value=ExamChoice.END_SEM.value),
         ]
     )
     async def past_paper(
         self,
         interaction: discord.Interaction[ProjectHyperlink],
-        exam: Choice[str],
+        exam: Choice[int],
         file: discord.Attachment,
         # TODO: add tags in the future
     ):
+        self.logger.info("Choice selected is %s", ExamChoice(exam).name)
         await interaction.response.defer(ephemeral=True, thinking=True)
