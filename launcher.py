579035764d2456970c5897880c35383c2207fa06
GetPsyched <priyanshutr@proton.me>
2023-06-13 19:19:40 +0400

    fix: remove redundant stuff

--- launcher.py
+++ launcher.py
@@ -1,55 +1,33 @@
 # Run this script for first time setups
 
-import asyncio
-import asyncpg
-import config
 import json
 import os
 import sqlite3
 
 try:
-    os.mkdir('db')
+    os.mkdir("db")
 except FileExistsError:
     pass
 
 # jsons
 jsons = {
-    'boards': {},
-    'emojis': {
-        'utility': {
-            'loading': '',
-            'no': '',
-            'not-verified': '',
-            'triggered': '',
-            'verified': '',
-            'yes': ''
-        },
-        'games': {}
-    },
-    'muted': [],
-    'self_roles': {},
-    'VCs': {
-        'vc_enabled_channels': [],
-        'party_vchannels': [],
-        'allow_text': [],
-        'text_enabled_channels': [],
-        'party_tchannels': {}
-    },
+    "muted": [],
+    "self_roles": {},
 }
 
 for json_name, content in jsons.items():
     try:
-        with open(f'db/{json_name}.json', 'r') as file:
+        with open(f"db/{json_name}.json", "r") as file:
             json.load(file)
     except FileNotFoundError:
-        with open(f'db/{json_name}.json', 'w') as file:
+        with open(f"db/{json_name}.json", "w") as file:
             json.dump(content, file)
 
 # SQLite db
-for name in ('links', 'self_roles'):
-    conn = sqlite3.connect(f'db/{name}.db')
+for name in ("self_roles",):
+    conn = sqlite3.connect(f"db/{name}.db")
     c = conn.cursor()
 
-    with open(f'utils/schemas/{name}.sql') as sql:
+    with open(f"utils/schemas/{name}.sql") as sql:
         c.executescript(sql.read())
         conn.commit()
