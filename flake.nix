330a694fbcbbae3a8897d8de21765a8b33917fbd
GetPsyched <priyanshu@getpsyched.dev>
2023-11-13 23:02:17 +0530

    feat: switch to flakey-devshells 0.2.3

--- flake.nix
+++ flake.nix
@@ -2,24 +2,24 @@
   description = "Project Hyperlink's development environment";
 
   inputs = {
-    python-env.url = "https://flakehub.com/f/GetPsyched/python-env/0.x.x.tar.gz";
-    python-env.inputs.nixpkgs.follows = "nixpkgs";
+    flakey-devShells.url = "https://flakehub.com/f/GetPsyched/not-so-flakey-devshells/0.x.x.tar.gz";
+    flakey-devShells.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs-fluent.url = "github:getpsyched/nixpkgs/python-fluent";
   };
 
-  outputs = inputs@{ nixpkgs, python-env, ... }:
+  outputs = inputs@{ nixpkgs, flakey-devShells, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       pkgs-fluent = inputs.nixpkgs-fluent.legacyPackages.${system};
-      python-env-pkgs = python-env.outputs.packages.${system};
+      flakey-devShell-pkgs = flakey-devShells.outputs.packages.${system};
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          python-env-pkgs.default
-          python-env-pkgs.vscode
+        nativeBuildInputs = with pkgs; [
+          (flakey-devShell-pkgs.default.override { environments = [ "nix" "python" ]; })
+          (flakey-devShell-pkgs.vscodium.override { environments = [ "nix" "python" ]; })
 
           railway
           pkgs-fluent.python311Packages.fluent-runtime
